3968bd5ee7b27f308b91f9ecef6d769a40180fc9
icxd <icoding666@gmail.com>
2024-12-07 20:29:38 +0100

    Fix bug in build-toolchain.sh

--- toolchain/build-toolchain.sh
+++ toolchain/build-toolchain.sh
@@ -9,7 +9,7 @@ export PATH=$PREFIX/bin:$PATH
 if [ ! -d binutils ]; then git clone git://sourceware.org/git/binutils-gdb.git binutils; fi
 if [ ! -d gcc ]; then git clone git://gcc.gnu.org/git/gcc.git gcc; fi
 
-mkdir binutils-build
+mkdir -p binutils-build
 cd binutils-build
 
 ../binutils/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
@@ -17,7 +17,7 @@ make -j$(nproc)
 make install
 
 cd ..
-mkdir gcc-build
+mkdir -p gcc-build
 cd gcc-build
 
 which -- $TARGET-as || echo $TARGET-as is not in PATH
